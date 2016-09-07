cd5db603052517c77a13f99d3faa9b2128ed813d
lujun9972 <lujun9972@gmail.com>
2016-09-08 00:32:37 +0800

    用多进程下载

--- download_comic_from_avbebe.sh
+++ download_comic_from_avbebe.sh
@@ -7,8 +7,9 @@ function download_imgs
     entry_content=$(echo $html|grep -Po -e '<div class="entry-content clearfix".+?</div>' )
     imgs=$(echo $entry_content |grep -Eo 'src="[^"]+"' |grep -Eo '".+"' |grep -Eo '[^"]+')
     for img in $imgs;do
-        curl -s --socks5 127.0.0.1:1080 --create-dirs -o $(basename $img) $img
+        curl -s --socks5 127.0.0.1:1080 --create-dirs -o $(basename $img) $img &
     done
+    wait
     # url=$(echo $html|grep -Eo '<a href="[^"]+">下一頁</a>' |grep -Eo '".+"') |grep -Eo '[^"]+'
     url=$(echo $html|grep -Eo '<a href="[^"]+">下一頁</a>' |grep -Eo '".+"' |grep -Eo '[^"]+')
     echo $url
