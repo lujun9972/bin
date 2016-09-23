ffaaf04f3a3712308d88fff48106d289f60908a2
lujun9972 <lujun9972@gmail.com>
2016-09-24 07:36:55 +0800

    update

--- download_comic_from_avbebe.sh
+++ download_comic_from_avbebe.sh
@@ -1,26 +1,33 @@
 #!/bin/bash
 
-function download_imgs
+function download_imgs_from
 {
-    url=$1
-    html=$(curl -s --socks5 127.0.0.1:1080 $url)
+    html="$@"
     entry_content=$(echo $html|grep -Po -e '<div class="entry-content clearfix".+?</div>' )
     imgs=$(echo $entry_content |grep -Eo 'src="[^"]+"' |grep -Eo '".+"' |grep -Eo '[^"]+')
     for img in $imgs;do
+        echo "downloading $img"
         curl -s --socks5 127.0.0.1:1080 --create-dirs -o $(basename $img) $img &
     done
     wait
-    # url=$(echo $html|grep -Eo '<a href="[^"]+">下一頁</a>' |grep -Eo '".+"') |grep -Eo '[^"]+'
+}
+
+function get_next_url_from
+{
+    html="$@"
     url=$(echo $html|grep -Eo '<a href="[^"]+">下一頁</a>' |grep -Eo '".+"' |grep -Eo '[^"]+')
     echo $url
 }
 
-
 url=$1
-html=$(curl -s --socks5 127.0.0.1:1080 $url)
-title=$(echo $html |egrep -o "<title>.+</title>"|sed 's/<title>\|<\/title>//g')
-mkdir "$title"
-cd "$title"
+title=""
 while [ "$url" != "" ];do
-    url=$(download_imgs $url)
+    html=$(curl -s --socks5 127.0.0.1:1080 $url)
+    if [ "$title" == "" ];then
+        title=$(echo $html |egrep -o "<title>.+</title>"|sed 's/<title>\|<\/title>//g')
+        mkdir "$title"
+        cd "$title"
+    fi
+    download_imgs_from $html
+    url=$(get_next_url_from $html)
 done
