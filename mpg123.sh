802987efbf8d655d410f0ff77c842369051fbf4a
darksun <lujun9972@gmail.com>
2016-10-23 15:01:20 +0800

    using curl and mpg123 to play https mpg3

---
+++ mpg123.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# play https mp3 using mpg123
+
+function https_url_p ()
+{
+    local url=$1
+    echo $url |egrep "^https://" >/dev/null
+}
+
+uri=$1
+if https_url_p $uri;then
+    curl $uri | mpg123 -
+else
+    mpg123 $curi
+fi
