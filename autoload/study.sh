bd448c836ebb78ff26474d6715de92b46cb079f5
darksun <lujun9972@gmail.com>
2016-11-04 13:11:13 +0800

    fix typo

--- autoload/study.sh
+++ autoload/study.sh
@@ -3,7 +3,7 @@ function study
     cd ~/study || exit 1
     dirName=$1
     if [ ! -z $dirName ];then
-	if [[ -d $dirName ]];1111
+	if [[ -d $dirName ]];then
 	    cd $dirName
 	else
 	    mkdir $dirName
