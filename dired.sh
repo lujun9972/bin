dadc8e4508beff737931a088186b4d5778e9ffc6
darksun <lujun9972@gmail.com>
2018-05-10 16:05:28 +0800

    自动kill-this-buffer

--- dired.sh
+++ dired.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/bash
 
-emacsclient.sh --eval "(progn (require 'dired) (dired \"$(pwd)\"))"
+emacsclient.sh --eval "(progn (require 'dired)
+                (dired \"$(pwd)\")
+                (local-set-key (kbd \"C-x C-c\")
+                   (lambda ()
+                       (interactive)
+                        (kill-this-buffer)
+                        (save-buffers-kill-terminal 't))))"
