2df585e0010248d85f6d6d32876b0566efa172aa
darksun <lujun9972@gmail.com>
2018-06-18 19:37:54 +0800

    update

--- dired.sh
+++ dired.sh
@@ -2,6 +2,7 @@
 
 emacsclient.sh --eval "(progn (require 'dired)
                 (dired \"$(pwd)\")
+		(delete-other-windows)
                 (local-set-key (kbd \"C-x C-c\")
                    (lambda ()
                        (interactive)
