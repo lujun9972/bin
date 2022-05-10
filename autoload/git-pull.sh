8f8e677e1b3e2327e128bf39af6083064521abb9
darksun <lujun9972@gmail.com>
2022-05-10 19:32:49 +0800

    update

--- autoload/git-pull.sh
+++ autoload/git-pull.sh
@@ -1,4 +1,4 @@
-for dir in ~/bin ~/我的笔记 ~/我的GTD ~/MyLisp ~/github/* ~/Daily
+for dir in ~/bin ~/我的笔记 ~/我的GTD ~/MyLisp ~/github/* ~/Daily ~/.spacemacs.d
 do
 if [ -d $dir ];then
 (echo $dir && cd $dir && git pull) &
